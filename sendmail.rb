1ee7805d665ebd711af569832ca2db8fe3fec95b
Tagaki Aki <a-tagaki@esm.co.jp>
2011-11-11 19:36:41 +0900

    initial

---
+++ sendmail.rb
@@ -0,0 +1,20 @@
+#coding: utf-8
+
+require 'yaml'
+require 'rubygems'
+require 'action_mailer'
+
+class GarbageMailer < ActionMailer::Base
+  def garbage_message(to_address,my_subject,my_body)
+    from 'akiinyo@esm.co.jp'
+    recipients to_address
+    subject my_subject
+    body my_body
+  end
+end
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = YAML.load_file('settings.yml')['smtp_settings'].symbolize_keys
+GarbageMailer.garbage_message('aki.hosecarioka@gmail.com','ゴミンゴからのお知らせ','今日はゴミの日です！').deliver
